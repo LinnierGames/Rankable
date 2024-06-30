172e493eae4b8ca6d4ccc76d7450f0530476026d
Erick Sanchez <esericksanc@gmail.com>
2024-06-29 23:03:03 -0700

    feature and tests!

--- Sources/Rankable/Rankable.swift
+++ Sources/Rankable/Rankable.swift
@@ -1,2 +1,161 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// https://www.appsdissected.com/order-core-data-entities-maximum-speed/
+
+// Setup rank (space all elements out) ✅
+// Move rank (move an element) ✅
+// Space out (make space for new rank, or space out all) ✅
+// Insert into rank (add new element into list and make space if needed) ✅
+// Reversed rank? Or just reverse the input?
+// multiple sections?
+// RankableObject and Rankable (value type)
+
+
+
+var kElementSpacing = 16
+//let copy = todos
+//copy[1].order = kElementSpacing * 0
+//copy[3].order = kElementSpacing * 1
+//copy[0].order = kElementSpacing * 2
+//copy[2].order = kElementSpacing * 3
+
+public protocol RankableObject: AnyObject {
+  var rank: Int { get set } // TODO: Make Index generic vs just Int
+}
+
+/// Space out all elements in the given collection
+public func spaceOutAllElements<Elements: Collection>(
+  _ elements: Elements
+) where Elements.Element: RankableObject {
+  for (offset, element) in elements.enumerated() {
+    element.rank = (offset + 1) * kElementSpacing
+  }
+}
+
+/// Space out starting at the given index until adjacent gaps are filled
+internal func spaceOut<Elements: Collection>(
+  _ elements: Elements,
+  atIndex index: Int,
+  reservation: Int? = nil
+) where Elements.Element: RankableObject {
+  var offset = index
+  var offsetIndex = elements.index(elements.startIndex, offsetBy: offset)
+  var offsetRank = elements[offsetIndex].rank
+
+  let lastIndex = elements.index(elements.endIndex, offsetBy: -1, limitedBy: elements.startIndex)
+
+  if offsetIndex == lastIndex {
+    elements[offsetIndex].rank = offsetRank + kElementSpacing
+  } else {
+    let afterOffsetIndex = elements.index(offsetIndex, offsetBy: 1)
+    let afterOffsetRank = elements[afterOffsetIndex].rank
+
+    if let reservation, reservation + kElementSpacing > afterOffsetRank {
+      spaceOut(
+        elements,
+        atIndex: offset + 1,
+        reservation: reservation + kElementSpacing
+      ) // FIXME: use indexes instead
+    }
+
+    let notEnoughSpaceAfterOffset = offsetRank + kElementSpacing > afterOffsetRank
+    if notEnoughSpaceAfterOffset {
+      spaceOut(
+        elements,
+        atIndex: offset + 1,
+        reservation: (reservation ?? offsetRank) + kElementSpacing
+      ) // FIXME: use indexes instead
+      elements[offsetIndex].rank = (reservation ?? offsetRank)
+    } else {
+      elements[offsetIndex].rank = (reservation ?? offsetRank)
+    }
+  }
+}
+
+/// Insert the source to the destination making space where needed
+///
+/// gap of 2
+/// A....B.CD.E.....F..G..H..I move item
+/// |....|.|__|__|...|..|..|..|
+///  start ^__|__^ end
+///
+/// A....B.C D.E..F.....G..H..I move item
+///         ^           <
+/// A....B.C[]DE..F.....G..H..I make space
+/// A....B.C..D[]EF.....G..H..I make more space
+/// A....B.C..D..EF.....G..H..I ready
+/// |....|.|__|__||.....|..|..|
+/// A....B.C.GD..EF........H..I move item
+public func moveElement<Elements: Collection>(
+  source: Int,
+  destination: Int,
+  elements: Elements
+) where Elements.Element: RankableObject, Elements.Index == Int {
+  guard source != destination else { return }
+  guard elements.count > 1 else { return }
+
+  let elementMovedUp = destination > source
+  let newRank = reserveRank(destination: destination, useSpacingAfterDestination: elementMovedUp, elements: elements)
+  elements[source].rank = newRank
+}
+
+/// Setup a rank at the given destination making space if needed
+///
+/// - Parameters:
+///   - destination: Index where to reserve a new rank
+///   - useSpacingAfterDestination: if true, reserve after destination. Otherwise, before destination
+///
+/// - Returns: non-zero rank for the new element
+public func reserveRank<Elements: Collection>(
+  destination: Int,
+  useSpacingAfterDestination: Bool = true,
+  elements: Elements
+) -> Int where Elements.Element: RankableObject, Elements.Index == Int {
+  guard !elements.isEmpty else { return kElementSpacing }
+  guard elements.indices.contains(destination) else {
+    return kElementSpacing * elements.count
+  }
+
+  let destinationIndex = elements.index(elements.startIndex, offsetBy: destination)
+
+  let movingElementAtStart = destinationIndex == elements.startIndex
+  let movingElementAtEnd = destinationIndex == elements.index(elements.endIndex, offsetBy: -1, limitedBy: elements.startIndex)
+
+  let leadingOrder: Int
+  let trailingOrder: Int
+
+  func reserve() -> Int {
+    let isThereSpaceBetweenOrders = trailingOrder - leadingOrder > 1
+    if isThereSpaceBetweenOrders {
+      let newRank = (leadingOrder + trailingOrder) / 2
+      return newRank
+    } else {
+      if useSpacingAfterDestination {
+        spaceOut(elements, atIndex: destination)
+      } else if movingElementAtStart {
+        spaceOut(elements, atIndex: destination)
+      } else {
+        spaceOut(elements, atIndex: destination - 1)
+      }
+
+      return reserveRank(destination: destination, useSpacingAfterDestination: useSpacingAfterDestination, elements: elements)
+    }
+  }
+
+  if movingElementAtEnd {
+    let lastRank = elements[destination].rank
+    return lastRank + kElementSpacing
+  } else if movingElementAtStart {
+    leadingOrder = .zero
+    trailingOrder = elements[destinationIndex].rank
+    return reserve()
+  } else {
+    if useSpacingAfterDestination {
+      leadingOrder = elements[destination].rank
+      trailingOrder = elements[destination + 1].rank
+    } else {
+      leadingOrder = elements[destination - 1].rank
+      trailingOrder = elements[destination].rank
+    }
+
+    return reserve()
+  }
+}
--- Tests/RankableTests/RankableTests.swift
+++ Tests/RankableTests/RankableTests.swift
@@ -1,12 +1,352 @@
 import XCTest
+
 @testable import Rankable
 
 final class RankableTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+  override class func setUp() {
+    kElementSpacing = 4
+  }
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+  func testSpacingOutAllElements() {
+    let todo1 = Todo(title: "1", order: 0)
+    let todo2 = Todo(title: "2", order: 0)
+    let todo3 = Todo(title: "3", order: 0)
+    let todo4 = Todo(title: "4", order: 0)
+    let elements = [todo1, todo2, todo3, todo4]
+
+    spaceOutAllElements(elements)
+    XCTAssertEqual(
+      elements.map(\.order),
+      [4, 8, 12, 16]
+    )
+  }
+
+  func testMovingElement_inside_movedUp() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo3 = Todo(title: "3", order: 8)
+    let todo2 = Todo(title: "2", order: 12)
+    let todo4 = Todo(title: "4", order: 16)
+    let elements = [
+      todo1, // 0
+      todo3, // 1
+      todo2, // 2
+      todo4, // 3
+    ]
+
+    moveElement(source: 1, destination: 2, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_inside_movedUp_adjacentRanks() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo3 = Todo(title: "3", order: 5)
+    let todo4 = Todo(title: "4", order: 50)
+    let todo2 = Todo(title: "2", order: 100)
+    let elements = [
+      todo1, // 0
+      todo3, // 1
+      todo4, // 2
+      todo2, // 3
+    ]
+
+    moveElement(source: 3, destination: 1, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.order),
+      [
+        kElementSpacing * 1,
+        (kElementSpacing + kElementSpacing * 2) / 2,
+        kElementSpacing * 2,
+        50,
+      ]
+    )
+  }
+
+  func testMovingElement_inside_movedUp_adjacentRanks_spaceOutMoreThanOnce() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo3 = Todo(title: "3", order: 5)
+    let todo4 = Todo(title: "4", order: 6)
+    let todo5 = Todo(title: "5", order: 7)
+    let todo6 = Todo(title: "6", order: 8)
+    let todo7 = Todo(title: "7", order: 50)
+    let todo2 = Todo(title: "2", order: 100)
+    let elements = [
+      todo1, // 0
+      todo3, // 1
+      todo4, // 2
+      todo5, // 3
+      todo6, // 4
+      todo7, // 5
+      todo2, // 6
+    ]
+
+    moveElement(source: 6, destination: 1, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4, 5, 6, 7].map(String.init)
+    )
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.order),
+      [
+        kElementSpacing * 1,
+        (kElementSpacing + kElementSpacing * 2) / 2,
+        kElementSpacing * 2,
+        kElementSpacing * 3,
+        kElementSpacing * 4,
+        kElementSpacing * 5,
+        50,
+      ]
+    )
+  }
+
+  func testMovingElement_inside_movedUp_adjacentRanks_spaceOutAllTheWay() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo3 = Todo(title: "3", order: 5)
+    let todo4 = Todo(title: "4", order: 6)
+    let todo5 = Todo(title: "5", order: 7)
+    let todo6 = Todo(title: "6", order: 8)
+    let todo7 = Todo(title: "7", order: 9)
+    let todo2 = Todo(title: "2", order: 10)
+    let elements = [
+      todo1, // 0
+      todo3, // 1
+      todo4, // 2
+      todo5, // 3
+      todo6, // 4
+      todo7, // 5
+      todo2, // 6
+    ]
+
+    moveElement(source: 6, destination: 1, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4, 5, 6, 7].map(String.init)
+    )
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.order),
+      [
+        kElementSpacing * 1,
+        (kElementSpacing + kElementSpacing * 2) / 2,
+        kElementSpacing * 2,
+        kElementSpacing * 3,
+        kElementSpacing * 4,
+        kElementSpacing * 5,
+        kElementSpacing * 6,
+      ]
+    )
+  }
+
+  func testMovingElement_inside_movedDown() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo3 = Todo(title: "3", order: 8)
+    let todo2 = Todo(title: "2", order: 12)
+    let todo4 = Todo(title: "4", order: 16)
+    let elements = [
+      todo1, // 0
+      todo3, // 1
+      todo2, // 2
+      todo4, // 3
+    ]
+
+    moveElement(source: 2,destination: 1,elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_inside_movedDown_adjacentRanks() {}
+  func testMovingElement_inside_movedDown_adjacentRanks_spaceOutMoreThanOnce() {}
+  func testMovingElement_inside_movedDown_adjacentRanks_spaceOutAllTheWay() {}
+
+  func testMovingElement_bounds_movedUp() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo2 = Todo(title: "2", order: 8)
+    let todo4 = Todo(title: "4", order: 12)
+    let todo3 = Todo(title: "3", order: 16)
+    let elements = [
+      todo1, // 0
+      todo2, // 1
+      todo4, // 2
+      todo3, // 3
+    ]
+
+    moveElement(source: 2, destination: 3, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_bounds_movedUp2() {
+    let todo1 = Todo(title: "1", order: 4)
+    let todo4 = Todo(title: "4", order: 8)
+    let todo2 = Todo(title: "2", order: 12)
+    let todo3 = Todo(title: "3", order: 16)
+    let elements = [
+      todo1, // 0
+      todo4, // 1
+      todo2, // 2
+      todo3, // 3
+    ]
+
+    moveElement(source: 1, destination: 3, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_bounds_movedUp_adjacentRanks() {}
+  func testMovingElement_bounds_movedUp_adjacentRanks_spaceOutMoreThanOnce() {}
+  func testMovingElement_bounds_movedUp_adjacentRanks_spaceOutAllTheWay() {}
+
+  func testMovingElement_bounds_movedDown() {
+    let todo2 = Todo(title: "2", order: 4)
+    let todo1 = Todo(title: "1", order: 8)
+    let todo3 = Todo(title: "3", order: 12)
+    let todo4 = Todo(title: "4", order: 16)
+    let elements = [
+      todo2, // 0
+      todo1, // 1
+      todo3, // 2
+      todo4, // 3
+    ]
+
+    moveElement(source: 1, destination: 0, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_bounds_movedDown2() {
+    let todo2 = Todo(title: "2", order: 4)
+    let todo3 = Todo(title: "3", order: 8)
+    let todo1 = Todo(title: "1", order: 12)
+    let todo4 = Todo(title: "4", order: 16)
+    let elements = [
+      todo2, // 0
+      todo3, // 1
+      todo1, // 2
+      todo4, // 3
+    ]
+
+    moveElement(source: 2, destination: 0, elements: elements)
+    XCTAssertEqual(
+      elements.sorted(by: { $0.order < $1.order }).map(\.title),
+      [1, 2, 3, 4].map(String.init)
+    )
+  }
+
+  func testMovingElement_bounds_movedDown_adjacentRanks() {}
+  func testMovingElement_bounds_movedDown_adjacentRanks_spaceOutMoreThanOnce() {}
+  func testMovingElement_bounds_movedDown_adjacentRanks_spaceOutAllTheWay() {}
+
+  func testReserve_emptyElements() {}
+
+  func tes() {
+
+    let todo1 = Todo(title: "1", order: 0)
+    let todo2 = Todo(title: "2", order: 0)
+    let todo3 = Todo(title: "3", order: 0)
+    let todo4 = Todo(title: "4", order: 0)
+    let todo5 = Todo(title: "5", order: 0)
+
+    let todo22 = Todo(title: "2", order: 0)
+
+    var todos: [Todo] {
+      [
+        todo2,
+        todo1,
+        todo5,
+        todo3,
+        todo4,
+      ].sorted(by: { $0.order < $1.order })
     }
+
+    // testMovingElement_bounds_movedDown_needsSpacingOut
+    moveElement(source: 4, destination: 0, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 4, destination: 0, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 4, destination: 0, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 4, destination: 0, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+    // testMovingElement_bounds_movedUp
+    moveElement(source: 0, destination: 4, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 0, destination: 4, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 0, destination: 4, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 0, destination: 4, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+    moveElement(source: 0, destination: 4, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+    // testInsertElement_inside
+    todo5.title = "2"
+    todo5.rank = reserveRank(destination: 2, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+    // testInsertElement_atStart
+    todo4.title = "0"
+    todo4.rank = reserveRank(destination: 0, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+    // testInsertElement_atEnd
+    todo3.title = "100"
+    todo3.rank = reserveRank(destination: todos.count - 1, elements: todos)
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+    //
+    todos.map(\.title).joined(separator: ",")
+    todos.map(\.order).map(String.init).joined(separator: ",")
+
+  }
+}
+
+class Todo {
+  var title = "Untitled"
+  var isCompleted = false
+
+  var order = 0
+
+  init(
+    title: String = "Untitled",
+    isCompleted: Bool = false,
+    order: Int
+  ) {
+    self.title = title
+    self.isCompleted = isCompleted
+    self.order = order
+  }
+}
+
+extension Todo: RankableObject {
+  var rank: Int {
+    get { order }
+    set { order = newValue }
+  }
 }
